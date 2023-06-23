8735ac1afd9cf5f97b101a918db3875e27c393e5
Stoyan Peev <stoyanilievpeev@gmail.com>
2023-06-23 13:34:52 +0100

    Add random index generator function

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 const zodiacSigns = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 const astrologicalEvents = ['Moon Phase: New Moon', 'Moon Phase: Full Moon', 'Planet in Retrograde: Mercury', 'Planet in Retrograde: Venus', 'Solar Eclipse'];
 const predictions = ['Today is a great day for new beginnings.', 'Be wary of potential challenges.', 'Your hard work will pay off today.', 'Today will bring a significant encounter.', 'Expect financial growth today.', 'You will find tranquility with a loved one.', 'Today is a perfect day for self-reflection.', 'Your dreams are more reachable than they seem.'];
+
+const getRandomIndex = array => {
+    return Math.floor(Math.random() * array.length);
+}
